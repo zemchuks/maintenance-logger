8730f4263d25d94449fd58aec7c7e2b320e73029
Bruno Austin <brunoaugustine2@gmail.com>
2021-01-07 19:43:42 +0100

    add log items and preloader from materialize

--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import LogItem from './LogItem'
+import Preloader from '../layout/Preloader'
 
 const Logs = () => {
     const [logs, setLogs] = useState([])
@@ -19,8 +20,9 @@ const Logs = () => {
         setLoading(false)
     }
     if(loading) {
-        return <h4>Loading...</h4>
+        return <Preloader />
     }
+    
     return (
         <ul className='collection with-header'>
             <li className='collection-header'>
